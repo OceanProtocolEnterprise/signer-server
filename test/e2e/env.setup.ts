ec34bce3091230d3cb6809759ec784b0169d1844
ahmedraza118 <ahmedrazachrtx@gmail.com>
2026-06-15 15:18:56 +0300

    fix e2e configs

--- test/e2e/env.setup.ts
+++ test/e2e/env.setup.ts
@@ -1,21 +1,19 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 process.env.NODE_ENV = 'test';
 
-process.env.PRIVATE_KEY =
-  process.env.PRIVATE_KEY || 'test-private-key';
+process.env.PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-process.env.NODE_URI_MAP =
-  process.env.NODE_URI_MAP ||
-  JSON.stringify({
-    '11155111': 'http://localhost:8545',
-  });
+process.env.NODE_URI_MAP = process.env.NODE_URI_MAP;
 
 process.env.AUTHENTIK_JWKS_URI =
-  process.env.AUTHENTIK_JWKS_URI || 'http://test';
+  process.env.AUTHENTIK_JWKS_URI;
 
-process.env.AUTHENTIK_ISSUER =
-  process.env.AUTHENTIK_ISSUER || 'http://test';
+process.env.AUTHENTIK_ISSUER = process.env.AUTHENTIK_ISSUER;
 
 process.env.AUTHENTIK_AUDIENCE =
-  process.env.AUTHENTIK_AUDIENCE || 'test-audience';
+  process.env.AUTHENTIK_AUDIENCE;
 
 process.env.PORT = process.env.PORT || '3001';
